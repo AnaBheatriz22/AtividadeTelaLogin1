09175b1d4a1c8500b2f65d00751a5199a7f76ea9
AnaBheatriz22 <bheatrisa979@gmail.com>
2025-09-06 23:17:19 -0300

    Primeiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
-import Login from '../components/Login'
+import Cadastro from '../components/cadastro'
 
 function App() {
 
   return (
     <div className='App'>
       <h1>Tela de Login</h1>
-      <Login />
-      <img src="/public/Goku.jpg" alt="goku" />
+      <Cadastro />
+     
     </div>
   )
 }
